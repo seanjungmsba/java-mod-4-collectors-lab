608206299f5c52fed09847ad376ea5979bc69da4
seanjungmsba <seanjeong1996@gmail.com>
2022-07-20 13:16:36 -0400

    Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class Main {
 
-    // modify this method
+    // modified this method
     public static long multiplySquaresOfNumbers(List<Integer> numbers) {
-        // your code here
-        return 0;
+        return (long)numbers.stream().map(num -> num*num).reduce(1, (a,b) -> a*b);
     }
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        String[] values = scanner.nextLine().split(" ");
-
-        List<Integer> numbers = new ArrayList<>();
-        for (String val : values) {
-            numbers.add(Integer.parseInt(val));
+        try (Scanner scanner = new Scanner(System.in);) {
+            while (true) {
+                System.out.print("Enter a list of numbers separated by white space: ");
+                String[] values = scanner.nextLine().split(" ");
+                List<Integer> numbers = new ArrayList<>();
+                for (String val : values) {
+                    numbers.add(Integer.parseInt(val));
+                }
+                long val = multiplySquaresOfNumbers(numbers);
+                System.out.println(val);
+            }
+        } catch (InputMismatchException e) {
+            e.getMessage();
+            System.err.println("ERROR: Enter valid inputs!");
         }
-
-        long val = multiplySquaresOfNumbers(numbers);
-
-        System.out.println(val);
-
-        scanner.close();
     }
+
 }
